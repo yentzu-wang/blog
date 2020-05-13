31b57f3a572034f247387dcd0aa14c14c7eaa914
Frank <frank7300228@gmail.com>
2020-05-13 17:35:39 +0800

    Finish 12-123

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react"
 import { createAppContainer } from "react-navigation"
 import { createStackNavigator } from "react-navigation-stack"
 import IndexScreen from "./src/screens/IndexScreen"
@@ -14,4 +15,8 @@ const navigator = createStackNavigator(
   }
 )
 
-export default createAppContainer(navigator)
+const App = createAppContainer(navigator)
+
+export default () => {
+  return <App />
+}
