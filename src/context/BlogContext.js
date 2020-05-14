a77231429b063842f7137eae59c4ce4211289e57
Frank <frank7300228@gmail.com>
2020-05-14 14:02:49 +0800

    Finish 12-126

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,18 @@ import React, { createContext } from "react"
 const BlogContext = createContext()
 
 const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>
+  const blogPosts = [
+    {
+      title: "Blog Post #1"
+    },
+    {
+      title: "Blog Post #2"
+    }
+  ]
+
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  )
 }
 
 export { BlogProvider }
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,20 @@
 import React, { useContext } from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, FlatList } from "react-native"
 import BlogContext from "../context/BlogContext"
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext)
+  const blogPosts = useContext(BlogContext)
 
   return (
     <View>
       <Text>IndexScreen</Text>
-      <Text>{value}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
     </View>
   )
 }
