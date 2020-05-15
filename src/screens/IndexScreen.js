7296d988d32ed50563ff301ae4bbbc9c52c7d710
Frank <frank7300228@gmail.com>
2020-05-15 15:15:59 +0800

    Finish 12-136

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -10,7 +10,7 @@ import {
 import { Feather } from "@expo/vector-icons"
 import { Context } from "../context/BlogContext"
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
   return (
@@ -21,14 +21,18 @@ const IndexScreen = () => {
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>
-                {item.title} - {item.id}
-              </Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                <Feather name="trash" style={styles.icon} />
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Show", { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather name="trash" style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
           )
         }}
       />
---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const ShowScreen = () => {
+  return (
+    <View>
+      <Text>Show Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ShowScreen
