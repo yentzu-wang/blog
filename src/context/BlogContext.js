67404085ce876272807bac8765ce35e6743b0da2
Frank <frank7300228@gmail.com>
2020-05-18 14:38:52 +0800

    Finish 12-144

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -41,5 +41,5 @@ const deleteBlogPost = dispatch => {
 export const { Context, Provider } = createDataContext(
   blogReducer,
   { addBlogPost, deleteBlogPost },
-  []
+  [{ title: "TEST POST", content: "TEST CONTENT", id: 1 }]
 )
