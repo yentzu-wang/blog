e5601f653226f9c261200dd3430aaab4ebe0aa73
Frank <frank7300228@gmail.com>
2020-05-18 10:50:19 +0800

    Finish 12-141

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from "react"
 import { StyleSheet, Text, View, TextInput, Button } from "react-native"
+import { Context } from "../context/BlogContext"
 
 const CreateScreen = () => {
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
+  const { addBlogPost } = useContext(Context)
 
   return (
     <View>
@@ -19,7 +21,10 @@ const CreateScreen = () => {
         value={content}
         onChangeText={text => setContent(text)}
       />
-      <Button title="Add Blog Post" />
+      <Button
+        title="Add Blog Post"
+        onPress={() => addBlogPost(title, content)}
+      />
     </View>
   )
 }
