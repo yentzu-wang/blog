76e9f5c990028680dd42454ea4a20ab41430dde8
Frank <frank7300228@gmail.com>
2020-05-18 14:32:21 +0800

    Finish 12-143

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native"
+import { EvilIcons } from "@expo/vector-icons"
 import { Context } from "../context/BlogContext"
 
 const ShowScreen = ({ navigation }) => {
@@ -12,10 +13,21 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({})
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    )
+  }
+}
 
 export default ShowScreen
